bc92e79827b3d0d309b7b2e466cb6897e75c3dca
Peter Edge <peter@centzy.com>
2014-03-27 13:03:41 -0700

    commit

--- spec/protocol_buffers_require_spec.rb
+++ spec/protocol_buffers_require_spec.rb
@@ -21,15 +21,21 @@ describe ProtocolBuffersRequire do
     three_foo = Centzy::Three::Foo.new(:string_1 => "three")
     three_foo.string_1.should == "three"
 
-    two_foo2 = Centzy::Two::Foo.new(:centzy_one_foo_9 => one_foo)
+    two_foo2 = Centzy::Two::Foo.new(
+      :string_1 => "two",
+      :centzy_one_foo_9 => one_foo
+    )
     two_foo2.centzy_one_foo_9.string_1.should == "one"
+    two_foo2.string_1.should == "two"
 
     three_foo2 = Centzy::Three::Foo.new(
+      :string_1 => "three",
       :centzy_one_foo_9 => one_foo,
       :centzy_two_foo_10 => two_foo
     )
     three_foo2.centzy_one_foo_9.string_1.should == "one"
     three_foo2.centzy_two_foo_10.string_1.should == "two"
+    three_foo2.string_1.should == "three"
 
     Centzy::One::ServiceOne.rpcs.map { |rpc| rpc.name.to_s }.should =~ ["one_one_one", "one_two_two" ]
     Centzy::Two::ServiceOne.rpcs.map { |rpc| rpc.name.to_s }.should =~ ["one_one_one", "one_two_two" ]
